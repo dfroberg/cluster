9f96a14e55ccdcc0f64008bc3132372581b114a5
Danny Froberg <danny.froberg@aml.ink>
2021-09-02 14:37:04 +0200

    Disable pilot

---
+++ hack/install-specific-storage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This script depends on setting up a TCP LoadBalancer first
+
+export USER=dfroberg
+export SERVER_IP=192.168.30.70
+export K3S_VERSION=v1.21.4+k3s1
+export LB=192.168.30.60
+
+export NEXT_SERVER_IP=$1
+echo "Storage 03"
+k3sup join \
+  --ip $NEXT_SERVER_IP \
+  --user $USER \
+  --server-user $USER \
+  --server-ip $SERVER_IP \
+  --k3s-version=$K3S_VERSION \
+  --k3s-extra-args="\
+--node-label storage=yes \
+"
+echo "Done."
