86a7175f096b0ce71074abd283a88c7b4fd4b58b
Danny Froberg <danny.froberg@aml.ink>
2022-01-18 12:58:34 +0100

    Mod

--- cluster/core/backup-system/benji/create-restores-from-latest.sh
+++ cluster/core/backup-system/benji/create-restores-from-latest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-benji ls 'status == "valid" and date >= "$1" and date <= "$2"'  | grep $1 | awk '{print "benji-restore-pvc --force "$4" "$6}' | sed 's/\//\ /g'
+benji ls 'status == "valid" and date >= "$1" and date <= "$2"'  | grep $1 | awk '{print "benji-restore-pvc --pvc-storage-class=ceph-block --force "$4" "$6}' | sed 's/\//\ /g'
