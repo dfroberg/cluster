ac644a827161b7dce0242945b21138417238b06b
Danny Froberg <danny.froberg@aml.ink>
2022-03-01 14:18:58 +0100

    Add some docs at top of file for posterity

--- provision/terraform/xcpnodes/files/vm-autostart.sh
+++ provision/terraform/xcpnodes/files/vm-autostart.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-
+# Save the script and quit the editor when you're happy with it.
+# Remember to set the script to be executable with `chmod a+x vm-autostart.sh`.
+# Edit `/etc/rc.d/rc.local` with your favourite editor.
+# At the bottom of the file, add a call to your newly create and executable script:
+# ```
+# `/root/vm-autostart.sh` 
+# ```
+# Save the file and quit the editor.
+# Make the rc.local script executable:
+# ```
+# `chmod a+x /etc/rc.d/rc.local` 
+# ```
+# Next time your host restarts, your vms should start automatically. Remember to test the script manually by shutting down all your VMs and then running the script in the shell to see that you didn't inadvertently introduce any errors.
+#
 # xe vm-list for name-label, add in start order
 vms=("postgres" "master01" "master03" "master03" "storage01" "storage02" "storage03" "worker01" "worker02" "worker03")
 wait=42s
