17b3a86b94b5b1bda599cf8d1c2825ee5baacc1f
Danny Froberg <danny.froberg@aml.ink>
2022-03-14 11:15:30 +0100

    Put in some sanity checks into backup-pvc.sh

--- cluster/core/backup-system/benji/scripts/backup-pvc.sh
+++ cluster/core/backup-system/benji/scripts/backup-pvc.sh
@@ -1,2 +1,15 @@
 #!/bin/bash -e
-benji-backup-pvc -n $1 && wget https://healthchecks.k8s.aml.ink/ping/$2 -q -T 10 -t 5 -O /dev/null
+NAMESPACE=$1
+HEALTHCHECKID=$2
+if [[ "$NAMESPACE" != "" ]]
+then
+    if [[ "$HEALTHCHECKID" != "" ]]
+    then
+        benji-backup-pvc -n $1 && wget https://healthchecks.k8s.aml.ink/ping/$2 -q -T 10 -t 5 -O /dev/null
+    else
+        benji-backup-pvc -n $1
+    fi
+else
+    echo "ERROR no namespace"
+    exit 1
+fi
