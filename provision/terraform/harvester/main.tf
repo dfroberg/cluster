0e811c48042edcbd6679e06a4bd128c821fad60f
Danny Froberg <danny.froberg@aml.ink>
2022-03-21 16:57:02 +0100

    Terraform plans for Proxmox 7.x Hypervisor

--- provision/terraform/harvester/main.tf
+++ provision/terraform/harvester/main.tf
@@ -60,5 +60,5 @@ resource "harvester_image" "ubuntu20" {
 
   display_name = "ubuntu20"
   source_type  = "download"
-  url          = "http://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.img"
+  url          = "http://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.iso"
 }
